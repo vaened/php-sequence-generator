02817e53aa519f4244d0a44438bf7dc91b523118
Enea Dhack <enea.so@live.com>
2022-08-30 16:36:37 -0500

    add base tests

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by enea dhack.
+ */
+
+declare(strict_types=1);
+
+namespace Vaened\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+}
