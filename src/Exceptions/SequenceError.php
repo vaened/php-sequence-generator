c405cbbbe7fab893dc1f222d2821f61b38275216
Enea Dhack <enea.so@live.com>
2022-08-30 16:32:54 -0500

    add generic error

---
+++ src/Exceptions/SequenceError.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by enea dhack.
+ */
+
+declare(strict_types=1);
+
+namespace Vaened\SequenceGenerator\Exceptions;
+
+use RuntimeException;
+
+class SequenceError extends RuntimeException
+{
+}
